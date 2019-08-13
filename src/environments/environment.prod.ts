35912d57cf3a448b78c2d653adc70fa39ae038cb
JosseAS <gandalf.gris.89@gmail.com>
2019-08-13 01:32:59 -0500

    Actualizacion Currency en Soles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDaMhJgP4sLI524Mlla1ZJtPJZPgTaTP9k',
-    authDomain: 'ingreso-egreso-3886b.firebaseapp.com',
-    databaseURL: 'https://ingreso-egreso-3886b.firebaseio.com',
-    projectId: 'ingreso-egreso-3886b',
-    storageBucket: 'ingreso-egreso-3886b.appspot.com',
-    messagingSenderId: '467085582351',
-    appId: '1:467085582351:web:b86fc9ad4a32e6b7'
+    apiKey: '<apiKey>',
+    authDomain: '<authDomain>',
+    databaseURL: '<databaseURL>',
+    projectId: '<projectId>',
+    storageBucket: '<storageBucket>',
+    messagingSenderId: '<messagingSenderId>',
+    appId: '<appId>'
   }
 };
